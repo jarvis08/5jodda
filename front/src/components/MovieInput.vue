3d6562643112446b9b3ab21c64721eab6163bcf2
ohsean93 <jeasuk93@gmail.com>
2019-11-27 14:35:38 +0900

    add_movie_create

--- front/src/components/MovieInput.vue
+++ front/src/components/MovieInput.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="5jodda-movieinput">
     <h2>영화 생성</h2>
-    <!-- <div class="form-group">
+    <div class="form-group">
+      <label for="title">영화번호</label>
+      <input v-model="Info.pk" type="text" id="moviePk" class="form-control" placeholder="영화 번호 입력하세요.">
+      <br>
       <label for="title">영화명</label>
       <input v-model="Info.title" type="text" id="title" class="form-control" placeholder="영화 제목을 입력하세요.">
       <br>
@@ -15,16 +18,67 @@
       <input v-model="Info.description" type="text" id="description" class="form-control" placeholder="줄거리를 입력하세요.">
       <br>
       <label for="genres">장르</label>
-      <input v-model="Info.genres" type="text" id="genres" class="form-control" placeholder="장르들을 입력하세요.">
-    </div> -->
-    <!-- <button @click="createMovie()" class="btn btn-primary">등록하기</button> -->
+      <div v-for="genre in genres2" :key="genre.pk" >
+        <input type="checkbox" :id="genre.name" :value="genre.pk" v-model="genres">
+        <label :for="genre.name">{{ genre.name }}</label>
+      </div>
+
+      <br>
+      <button @click="createMovie()" class="btn btn-primary">등록하기</button>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'MovieInput',
-  Info: {
+  data() {
+    return {
+      Info: {},
+      genres: [],
+      genres2: [],
+      checkedNames: []
+    }
+  },
+  methods: {
+    findGenre(genreNum) {
+
+      const genreName = this.genres2[genreNum-1]
+
+      return genreName
+    },
+    getGenres() {
+      const token = this.$session.get('jwt')
+      const options = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+      
+      axios.get('http://127.0.0.1:8000/api/v1/movies/genres/', options)
+      .then(res => {
+        this.genres2 = res.data
+        })
+    },
+    createMovie() {
+      const token = this.$session.get('jwt')
+      const inputData = this.Info
+      inputData.genres = this.genres
+      
+      axios({
+        method: 'GET',
+        url: 'http://127.0.0.1:8000/api/v1/movies/create/',
+        data: inputData,
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      })
+    },
+  },
+  mounted() {
+    this.getGenres()
   }
 }
 </script>
