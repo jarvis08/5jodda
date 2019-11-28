c063e85ab685190c684b5cc97b18ffad31bb191c
오재석 <jeasuk93@gmail.com>
2019-11-28 21:23:01 +0900

    user_fix

---
+++ front/src/views/SearchUser.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container col-4 class="white--text justify-center mt-12">
+    <h2 class="mb-3 text-center">My Page</h2>
+    <label for="userName">사용자 이름</label>
+    <p class="form-control">{{ user.username }}</p>
+    <label for="email">이메일 주소</label>
+    <p class="form-control">{{ user.email }}</p>
+    <br>
+    <h4 class="mt-6">리뷰 목록</h4>
+    <table class="table table-striped white--text">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">content</th>
+          <th scope="col">작성자</th>
+          <th scope="col">delete</th>
+        </tr>
+      </thead>
+      <tbody v-for="(review, index) in user.review_set" :key="review.pk">
+        <tr>
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ review.content }}</td>
+          <td>{{ review.user_id }}</td>
+          <td><button>delete</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      user: {},
+      userNum: 0
+    }
+  },
+  methods: {
+    getUser() {
+      const token = this.$session.get('jwt')
+      const options = {
+        headers: {
+          Authorization: 'JWT ' + token
+        }
+      }
+      
+      axios.get(`http://127.0.0.1:8000/api/v1/users/${ this.userNum }/`, options)
+      .then(res => {
+        this.user = res.data
+      })
+    },
+  },
+  mounted() {
+    this.userNum = this.$route.params.userNum 
+    this.getUser()
+  }
+}
+</script>
+
+<style>
+
+</style>
